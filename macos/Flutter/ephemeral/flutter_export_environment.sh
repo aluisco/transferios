a3d2af3e21ac41b43a3a2bfdd44965ea514160b7
Aluisco Miguel Ricardo Mastrapa <aluisco2005@gmail.com>
2023-06-02 13:15:52 -0400

    Flutter upgraded to 3.10.2

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/flutter"
+export "FLUTTER_ROOT=/home/aluisco/.local/share/flutter"
 export "FLUTTER_APPLICATION_PATH=/home/aluisco/Projects/transferios"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
